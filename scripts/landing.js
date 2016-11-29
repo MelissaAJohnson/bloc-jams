3873629d54764d63a02c40e154fef1d30d55afce
melissajstudent <missyj2007@gmail.com>
2016-11-28 20:19:46 -0600

    Assignment 23 initial try

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,9 +2,6 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
 
-
-
-
    var revealFirstPoint = function() {
        points[0].style.opacity = 1;
        points[0].style.transform = "scaleX(1) translateY(0)";
